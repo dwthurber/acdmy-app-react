6b34fc0058a0fe69321005ba858616225dc8d2ee
Derek Thurber <derek.thurber@northwestern.edu>
2017-07-08 17:52:57 -0500

    Added collaborations container

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import './App.css';
 import { Grid } from 'semantic-ui-react';
 import Nav from './components/header/nav'
 import Video from './components/video/video'
+import CollaborationContainer from './components/collaborations/container'
 
 class App extends Component {
   render() {
     return (
-      <Grid padded>
-        <Nav />
-        <Video />
-      </Grid>
+      <div className="fullHeight">
+        <Grid padded>
+          <Nav />
+          <Video />
+        </Grid>
+        <CollaborationContainer />
+      </div>
     );
   }
 }
---
+++ src/components/collaborations/container.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Segment } from 'semantic-ui-react';
+
+class CollaborationContainer extends Component {
+  render() {
+    return (
+      <Segment textAlign='center' className="collaborationsContainer">
+          <Segment className="verticalMiddle">
+            This is Empty!
+          </Segment>
+      </Segment>
+    );
+  }
+}
+
+export default CollaborationContainer;
