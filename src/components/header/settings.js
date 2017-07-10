b3426751b5838a3f6fde736a90d229c80d7b24a8
Derek Thurber <derek.thurber@northwestern.edu>
2017-07-10 09:10:05 -0500

    updated ui components

--- src/components/header/settings.js
+++ src/components/header/settings.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
-import { Image, Icon } from 'semantic-ui-react';
+import { Image, Icon, Popup, Message } from 'semantic-ui-react';
 import user from '../../images/user.svg';
 
 const Settings = () => (
   <div>
-    <Icon name='alarm' circular size='small' color='grey' />
+    <Popup
+      trigger={<Icon link name='alarm' circular size='small' color='grey' />}
+      on='click'
+      position='bottom right'
+    >
+      <Popup.Header>Notifications </Popup.Header>
+      <Popup.Content>
+        No notifications currently
+      </Popup.Content>
+    </Popup>
+
     <Icon name='help' circular size='small' color='grey' />
     <Image src={user} height="20" avatar spaced='left' />
   </div>
