af4ad31c4ca07a2160c1882f7bed3b891e16e7c0
Derek Thurber <derek.thurber@northwestern.edu>
2017-07-17 17:04:47 -0500

    fixed vertical alignment issues

--- src/components/header/settings.js
+++ src/components/header/settings.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Image, Icon, Popup, Message, Item } from 'semantic-ui-react';
+import { Image, Icon, Popup, List } from 'semantic-ui-react';
 import user from '../../images/user.svg';
 import Help from './help'
 
@@ -9,41 +9,53 @@ const Notifications = {
   textAlign: 'center'
 }
 
+const MicrophoneStyle = {
+  position: 'relative',
+  bottom: '4'
+}
+
+const SmallMargin = {
+  marginLeft: '4'
+}
+
 const Settings = () => (
-  <Item.Group>
-    <Item>
-      <Item.Content verticalAlign='middle'>
-        <Popup
-          trigger={<Icon link name='alarm' circular size='small' spaced='right' color='grey' />}
-          on='click'
-          position='bottom right'
-          offset={10}
-          wide
-          style={Notifications}
-        >
-          <Popup.Content>
-            You are all up to date!
-          </Popup.Content>
-        </Popup>
+  <List horizontal verticalAlign='middle'>
+    <List.Item>
+      <Popup
+        trigger={<Icon link name='alarm' circular size='small' color='grey' />}
+        on='click'
+        position='bottom right'
+        offset={10}
+        wide
+        style={Notifications}
+      >
+        <Popup.Content>
+          You are all up to date!
+        </Popup.Content>
+      </Popup>
+    </List.Item>
 
-        <Help />
-
-        <Icon link name='microphone' circular className="primaryIcon" spaced='left' />
-        <Popup
-          trigger={<Image href='#' src={user} height="20" avatar  />}
-          on='click'
-          position='bottom right'
-          offset={5}
-          wide
-          style={Notifications}
-        >
-          <Popup.Content>
-            Name
-          </Popup.Content>
-        </Popup>
-      </Item.Content>
-    </Item>
-  </Item.Group>
+    <List.Item  style={SmallMargin}>
+      <Help />
+    </List.Item>
+    <List.Item style={SmallMargin}>
+      <Icon link name='microphone' circular style={MicrophoneStyle} className="primaryIcon" />
+    </List.Item>
+    <List.Item style={SmallMargin}>
+      <Popup
+        trigger={<Image href='#' src={user} height="20" avatar  />}
+        on='click'
+        position='bottom right'
+        offset={5}
+        wide
+        style={Notifications}
+      >
+        <Popup.Content>
+          Name
+        </Popup.Content>
+      </Popup>
+    </List.Item>
+  </List>
 )
 
 export default Settings
